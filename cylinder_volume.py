63c89d3b4eef11327b7ab2f25afebe32e4b96ad3
apuvvala-git <apuvvala@gmail.com>
2018-01-31 20:48:37 -0500

    Add files via upload

---
+++ cylinder_volume.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 20:36:44 2018
+
+@author: Ajay
+"""
+
+def cylinder_volume (height, radius):
+    pi = 3.14159
+    return height * pi * radius**2
